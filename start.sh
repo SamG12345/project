bcd8698f82eefc0df207548b521ff8de6342dbb2
SamG12345 <101618721+SamG12345@users.noreply.github.com>
2026-07-11 17:38:36 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,13 @@
 
 sudo sed -i "s|http://de.archive.ubuntu.com/ubuntu/|http://archive.ubuntu.com/ubuntu/|" /etc/apt/sources.list
 sudo apt update && \
-sudo apt install -y docker.io openssh-server net-tools nmap && \
+sudo apt install -y curl docker.io openssh-server net-tools nmap && \
 snap install kubectl && \
-snap install minikube && \
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64 && \
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64 
+
 minikube start
+
 
 
 bash install_docker.sh
