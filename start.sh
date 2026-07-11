5f1dbe26226ba091df393435a182cff66d16e53f
SamG12345 <101618721+SamG12345@users.noreply.github.com>
2026-07-11 17:16:38 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 sudo sed -i "s|http://de.archive.ubuntu.com/ubuntu/|http://archive.ubuntu.com/ubuntu/|" /etc/apt/sources.list
 sudo apt update && \
-apt install -y docker.io openssh-server net-tools nmap && \
+sudo apt install -y docker.io openssh-server net-tools nmap && \
 snap install kubectl && \
 snap install minikube && \
 minikube start
